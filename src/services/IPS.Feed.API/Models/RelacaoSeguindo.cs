54f38c8a083daad30f3ca55776ca516556c014d6
Andrey Dario <andreydario26@gmail.com>
2023-08-20 14:14:01 -0300

    #Issue3 - Relacionamento Seguidores - Feature

---
+++ src/services/IPS.Feed.API/Models/RelacaoSeguindo.cs
@@ -0,0 +1,10 @@
+﻿using IPS.Core.DomainObjects;
+
+namespace IPS.Feed.API.Models
+{
+    public class RelacaoSeguindo : Entity
+    {
+        public Guid IdUsuario { get; set; }
+        public Guid IdSeguidores { get; set; }
+    }
+}
