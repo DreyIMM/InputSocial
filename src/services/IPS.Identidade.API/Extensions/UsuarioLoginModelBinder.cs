f4d0fe667a0a744690d046c56497d07d16d70406
Andrey Dario <andreydario26@gmail.com>
2023-10-13 21:12:57 -0300

    #Issue11 - Inclusao de imagem - Feature

---
+++ src/services/IPS.Identidade.API/Extensions/UsuarioLoginModelBinder.cs
@@ -0,0 +1,31 @@
+﻿using IPS.Identidade.API.Models;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.Text.Json;
+
+namespace IPS.Identidade.API.Extensions
+{
+    public class UsuarioLoginModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            if (bindingContext == null)
+            {
+                throw new ArgumentNullException(nameof(bindingContext));
+            }
+
+            var serializeOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                PropertyNameCaseInsensitive = true
+            };
+
+            var usuario = JsonSerializer.Deserialize<UsuarioRegistro>(bindingContext.ValueProvider.GetValue("UsuarioRegistro").FirstOrDefault(), serializeOptions);
+            usuario.fotoPerfil = bindingContext.ActionContext.HttpContext.Request.Form.Files.FirstOrDefault();
+
+            bindingContext.Result = ModelBindingResult.Success(usuario);
+            return Task.CompletedTask;
+        }
+
+    }
+}
