53c323e3898a3c8fdffb4d324a95344b8ecabb97
Andrey Dario <andrey.barbosa@ons.org.br>
2023-11-12 23:03:13 -0300

    NLP com Python e SpaCy

---
+++ src/NLP/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import spacy
+
+app = Flask(__name__)
+
+nlp = spacy.load("pt_core_news_lg")
+
+@app.route('/processar_mensagem', methods=['POST'])
+def processar_mensagem():
+    try:
+        data = request.get_json()
+
+        if 'mensagem' not in data:
+            return jsonify({'error': 'A chave "mensagem" não está presente no JSON'}), 400
+
+        mensagem = data['mensagem']
+
+        doc = nlp(mensagem)
+
+        entidades = [{'texto': ent.text, 'tipo': ent.label_} for ent in doc.ents]
+
+        return jsonify({'entidades': entidades})
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
