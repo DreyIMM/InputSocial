f807964dbc6a1237c54baa550366f556afede9f1
DreyIMM <andreydario26@gmail.com>
2023-12-09 23:22:26 -0300

    Criando EndPoint Evento - Feature

---
+++ src/services/IPS.Feed.API/Controllers/EventoController.cs
@@ -0,0 +1,23 @@
+﻿using IPS.Feed.API.DTO;
+using IPS.WebApi.Core.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace IPS.Feed.API.Controllers
+{
+
+    [Route("api/evento")]
+    [Authorize]
+    public class EventoController : MainController
+    {
+
+        
+
+
+
+
+
+
+    }
+}
